ff3c1acdbb487083bfcef061478fb940d02fbeb7
Tran Quang Dat <qdatqb@gmail.com>
2021-01-28 15:36:53 +0700

    first commit

---
+++ lab1/src/main/java/com/example/lab1/MainActivity.kt
@@ -0,0 +1,67 @@
+    package com.example.lab1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    lateinit var diceImage : ImageView
+    lateinit var diceImage1 : ImageView
+    lateinit var diceImage2 : ImageView
+    lateinit var resultText : TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        diceImage = findViewById(R.id.dice_image)
+        diceImage1 = findViewById(R.id.dice_image1)
+        diceImage2 = findViewById(R.id.dice_image2)
+        resultText = findViewById(R.id.result_text)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        val resetButton : Button = findViewById(R.id.reset_button)
+        rollButton.setOnClickListener { rollDice() }
+        resetButton.setOnClickListener { resetDice() }
+    }
+    private fun rollDice() {
+        val randomInt = (1..6).random()
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        val randomInt1 =(1..6).random()
+        val drawableResource1 = when (randomInt1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        val randomInt2 =(1..6).random()
+        val drawableResource2 = when (randomInt2) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+        diceImage1.setImageResource(drawableResource1)
+        diceImage2.setImageResource(drawableResource2)
+        val result = randomInt + randomInt1 + randomInt2
+        resultText.text = result.toString()
+    }
+    private fun resetDice() {
+        resultText.text = "Tran Quang Dat"
+        diceImage.setImageResource(R.drawable.dice_1)
+        diceImage1.setImageResource(R.drawable.dice_1)
+        diceImage2.setImageResource(R.drawable.dice_1)
+    }
+}
