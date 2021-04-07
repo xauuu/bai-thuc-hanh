fbc4f2efd17feaef4284f0aa301ee9a017a407d4
Tran Quang Dat <qdatqb@gmail.com>
2021-04-07 16:43:08 +0700

    Bai thuc hanh 5

---
+++ GuessTheWord/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.android.guesstheword.screens.score
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+/**
+ * ViewModel for the final screen showing the score
+ */
+class ScoreViewModel(finalScore: Int) : ViewModel() {
+
+    // The final score
+    var score = finalScore
+
+    init {
+        Log.i("ScoreViewModel", "Final score is $finalScore")
+    }
+}
