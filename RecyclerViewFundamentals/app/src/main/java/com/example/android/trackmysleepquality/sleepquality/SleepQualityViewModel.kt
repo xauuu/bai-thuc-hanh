5d3b9e620a4282f810e0cc5bf14cab3c2892b509
Tran Quang Dat <qdatqb@gmail.com>
2021-05-06 14:43:02 +0700

    Bai thuc hanh 7

---
+++ RecyclerViewFundamentals/app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityViewModel.kt
@@ -0,0 +1,36 @@
+
+package com.example.android.trackmysleepquality.sleepquality
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.android.trackmysleepquality.database.SleepDatabaseDao
+import kotlinx.coroutines.launch
+
+class SleepQualityViewModel(
+        private val sleepNightKey: Long = 0L,
+        dataSource: SleepDatabaseDao) : ViewModel() {
+
+    val database = dataSource
+
+    private val _navigateToSleepTracker = MutableLiveData<Boolean?>()
+
+    val navigateToSleepTracker: LiveData<Boolean?>
+        get() = _navigateToSleepTracker
+
+    fun doneNavigating() {
+        _navigateToSleepTracker.value = null
+    }
+
+
+    fun onSetSleepQuality(quality: Int) {
+        viewModelScope.launch {
+                val tonight = database.get(sleepNightKey)
+                tonight.sleepQuality = quality
+                database.update(tonight)
+
+            _navigateToSleepTracker.value = true
+        }
+    }
+}
